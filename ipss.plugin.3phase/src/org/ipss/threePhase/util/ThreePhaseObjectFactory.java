fab30fb22085fae2e7eb839fa3735d165437b2b5
Tony Huang <eetonyhuang@gmail.com>
2015-03-31 16:17:45 -0700

    add 3phase dstab ODM Mapper and the test case

--- ipss.plugin.3phase/src/org/ipss/threePhase/util/ThreePhaseObjectFactory.java
+++ ipss.plugin.3phase/src/org/ipss/threePhase/util/ThreePhaseObjectFactory.java
@@ -1,5 +1,6 @@
 package org.ipss.threePhase.util;
 
+import org.interpss.numeric.NumericConstant;
 import org.ipss.threePhase.basic.Branch3Phase;
 import org.ipss.threePhase.basic.Bus3Phase;
 import org.ipss.threePhase.basic.Gen3Phase;
@@ -14,12 +15,38 @@ import org.ipss.threePhase.basic.impl.Transformer3PhaseImpl;
 import org.ipss.threePhase.dynamic.DStabGen3Phase;
 import org.ipss.threePhase.dynamic.DStabNetwork3Phase;
 import org.ipss.threePhase.dynamic.impl.DStabGen3PhaseImpl;
+import org.ipss.threePhase.dynamic.impl.DStabNetwork3phaseImpl;
 
 import com.interpss.DStabObjectFactory;
+import com.interpss.common.datatype.Constants;
 import com.interpss.common.exp.InterpssException;
+import com.interpss.core.aclf.netAdj.AclfNetAdjustment;
+import com.interpss.core.aclf.netAdj.NetAdjustFactory;
+import com.interpss.core.acsc.AcscFactory;
+import com.interpss.core.acsc.BusScCode;
+import com.interpss.core.acsc.BusScGrounding;
+import com.interpss.core.net.Branch;
+import com.interpss.core.net.OriginalDataFormat;
+import com.interpss.dstab.StaticLoadModel;
 
 public class ThreePhaseObjectFactory {
 	
+	public static  DStabNetwork3Phase create3PhaseDStabNetwork() {
+	       DStabNetwork3Phase net = new DStabNetwork3phaseImpl();
+	        
+	       //The following is copied from the DStabObjectFactory
+	        AclfNetAdjustment netAdj = NetAdjustFactory.eINSTANCE.createAclfNetAdjustment();
+			net.setAclfNetAdjust(netAdj);
+			netAdj.setAclfNet(net);
+			net.setId("undefined");
+			net.setOriginalDataFormat(OriginalDataFormat.IPSS_API);
+			net.setNetEqnIterationNoEvent(Constants.DStabNetItrNoEvent);
+			net.setNetEqnIterationWithEvent(Constants.DStabNetItrWithEvent);
+			net.setStaticLoadModel(StaticLoadModel.CONST_Z);
+			net.setStaticLoadSwitchVolt(Constants.DStabStaticLoadSwithVolt);
+			net.setStaticLoadSwitchDeadZone(Constants.DStabStaticLoadSwithDeadband);
+	      return net;
+	}
 	public static Transformer3Phase create3PXformer(){
 	   Transformer3Phase ph3Xfr = new Transformer3PhaseImpl();
 	   return ph3Xfr;
@@ -27,7 +54,16 @@ public class ThreePhaseObjectFactory {
 	
 	public static Bus3Phase create3PBus(String busId, DStabNetwork3Phase net) throws InterpssException{
 		Bus3Phase bus = new Bus3PhaseImpl();
+	  
+		//The following is copied from the DStabObjectFactory
 		bus.setId(busId);
+		BusScGrounding g = AcscFactory.eINSTANCE.createBusScGrounding();
+  		bus.setId(busId);
+  		bus.setScCode(BusScCode.NON_CONTRI);
+  		bus.setScGenZ1(NumericConstant.LargeBusZ);
+  		bus.setScGenZ0(NumericConstant.LargeBusZ);
+  		bus.setScGenZ2(NumericConstant.LargeBusZ);
+  		bus.setGrounding(g);
 		bus.setBusFreqMeasureBlock(DStabObjectFactory.createBusFreqMeasurement());
 		
 		net.addBus(bus);
@@ -57,6 +93,10 @@ public class ThreePhaseObjectFactory {
 		Load3Phase load = new Load3PhaseImpl();
 		load.setId(loadId);
 		return load;
+	}
+	public static Branch3Phase create3PBranch() {
+		Branch3Phase branch = new Branch3PhaseImpl();
+		return branch;
 	}
 
 }
