826032f3a9b3c89acc565225b630e7ab08b77638
Tony Huang <eetonyhuang@gmail.com>
2015-03-31 17:40:33 -0700

    update test case

--- ipss.plugin.3phase/src/org/ipss/threePhase/test/TestODM3PhaseDstabMapper.java
+++ ipss.plugin.3phase/src/org/ipss/threePhase/test/TestODM3PhaseDstabMapper.java
@@ -14,12 +14,15 @@ import org.interpss.mapper.odm.ODMDStabParserMapper;
 import org.interpss.numeric.util.PerformanceTimer;
 import org.interpss.util.FileUtil;
 import org.ipss.threePhase.dynamic.DStabNetwork3Phase;
+import org.ipss.threePhase.dynamic.DynamicEventProcessor3Phase;
 import org.ipss.threePhase.odm.ODM3PhaseDStabParserMapper;
 import org.junit.Test;
 
+import com.interpss.DStabObjectFactory;
 import com.interpss.SimuObjectFactory;
 import com.interpss.common.exp.InterpssException;
 import com.interpss.common.util.IpssLogger;
+import com.interpss.core.acsc.fault.SimpleFaultCode;
 import com.interpss.core.algo.LoadflowAlgorithm;
 import com.interpss.dstab.DStabilityNetwork;
 import com.interpss.dstab.algo.DynamicSimuAlgorithm;
@@ -31,7 +34,7 @@ import com.interpss.simu.SimuCtxType;
 public class TestODM3PhaseDstabMapper {
 	
 	@Test
-	public void test_IEEE9Bus_Dstab() throws InterpssException{
+	public void test_IEEE9Bus_3phase_Dstab() throws InterpssException{
 		IpssCorePlugin.init();
 		IpssCorePlugin.setLoggerLevel(Level.INFO);
 		PSSEAdapter adapter = new PSSEAdapter(PsseVersion.PSSE_30);
@@ -76,7 +79,7 @@ public class TestODM3PhaseDstabMapper {
 		dsNet.setNetEqnIterationNoEvent(1);
 		dsNet.setNetEqnIterationWithEvent(1);
 		dstabAlgo.setRefMachine(dsNet.getMachine("Bus1-mach1"));
-		//dsNet.addDynamicEvent(create3PhaseFaultEvent("Bus5",dsNet,1.0d,0.05),"3phaseFault@Bus5");
+		dsNet.addDynamicEvent(DStabObjectFactory.createBusFaultEvent("Bus5",dsNet,SimpleFaultCode.GROUND_LG,1.0d,0.05),"3phaseFault@Bus5");
         
 		
 		StateMonitor sm = new StateMonitor();
@@ -90,7 +93,94 @@ public class TestODM3PhaseDstabMapper {
 		
 		PerformanceTimer timer = new PerformanceTimer(IpssLogger.getLogger());
 		
+        // Must use this dynamic event process to modify the YMatrixABC
+		dstabAlgo.setDynamicEventHandler(new DynamicEventProcessor3Phase());
+		
+		if (dstabAlgo.initialization()) {
+			System.out.println(dsNet.getMachineInitCondition());
+			
+			System.out.println("Running DStab simulation ...");
+			timer.start();
+			dstabAlgo.performSimulation();
+			
+			timer.logStd("total simu time: ");
+			}
+			
 
+		
+		System.out.println(sm.toCSVString(sm.getMachAngleTable()));
+		
+	     System.out.println(sm.toCSVString(sm.getMachPeTable()));
+		
+//		FileUtil.writeText2File("output/ieee9_bus5_machPe_v5_03172015.csv",sm.toCSVString(sm.getMachPeTable()));
+//		FileUtil.writeText2File("output/ieee9_bus5_machAngle_v5_03172015.csv",sm.toCSVString(sm.getMachAngleTable()));
+//		FileUtil.writeText2File("output/ieee9_bus5_machSpd_v5_03172015.csv",sm.toCSVString(sm.getMachSpeedTable()));
+//		FileUtil.writeText2File("output/ieee9_bus5_busVolt_v5_03172015.csv",sm.toCSVString(sm.getBusVoltTable()));
+//		
+	}
+	
+	
+	@Test
+	public void test_IEEE9Bus_posSeq_Dstab() throws InterpssException{
+		IpssCorePlugin.init();
+		IpssCorePlugin.setLoggerLevel(Level.INFO);
+		PSSEAdapter adapter = new PSSEAdapter(PsseVersion.PSSE_30);
+		assertTrue(adapter.parseInputFile(NetType.DStabNet, new String[]{
+				"testData/IEEE9Bus/ieee9.raw",
+				"testData/IEEE9Bus/ieee9.seq",
+				//"testData/IEEE9Bus/ieee9_dyn_onlyGen_saturation.dyr"
+				"testData/IEEE9Bus/ieee9_dyn_onlyGen.dyr"
+		}));
+		DStabModelParser parser =(DStabModelParser) adapter.getModel();
+		
+		//System.out.println(parser.toXmlDoc());
+
+		
+		
+		SimuContext simuCtx = SimuObjectFactory.createSimuNetwork(SimuCtxType.DSTABILITY_NET);
+		if (!new ODMDStabParserMapper(IpssCorePlugin.getMsgHub())
+					.map2Model(parser, simuCtx)) {
+			System.out.println("Error: ODM model to InterPSS SimuCtx mapping error, please contact support@interpss.com");
+			return;
+		}
+		
+		
+	    DStabilityNetwork dsNet =simuCtx.getDStabilityNet();
+	    
+	    
+	    // build sequence network
+//	    SequenceNetworkBuilder seqNetHelper = new SequenceNetworkBuilder(dsNet,true);
+//	    seqNetHelper.buildSequenceNetwork(SequenceCode.NEGATIVE);
+//	    seqNetHelper.buildSequenceNetwork(SequenceCode.ZERO);
+//	    
+//	    System.out.println(dsNet.net2String());
+	    
+		DynamicSimuAlgorithm dstabAlgo = simuCtx.getDynSimuAlgorithm();
+		LoadflowAlgorithm aclfAlgo = dstabAlgo.getAclfAlgorithm();
+		assertTrue(aclfAlgo.loadflow());
+		System.out.println(AclfOutFunc.loadFlowSummary(dsNet));
+		
+		dstabAlgo.setSimuMethod(DynamicSimuMethod.MODIFIED_EULER);
+		dstabAlgo.setSimuStepSec(0.005d);
+		dstabAlgo.setTotalSimuTimeSec(3);
+		dsNet.setNetEqnIterationNoEvent(1);
+		dsNet.setNetEqnIterationWithEvent(1);
+		dstabAlgo.setRefMachine(dsNet.getMachine("Bus1-mach1"));
+		dsNet.addDynamicEvent(DStabObjectFactory.createBusFaultEvent("Bus5",dsNet,SimpleFaultCode.GROUND_LG,1.0d,0.05),"3phaseFault@Bus5");
+        
+		
+		StateMonitor sm = new StateMonitor();
+		sm.addGeneratorStdMonitor(new String[]{"Bus1-mach1","Bus2-mach1","Bus3-mach1"});
+		sm.addBusStdMonitor(new String[]{"Bus5","Bus4","Bus1"});
+		// set the output handler
+				dstabAlgo.setSimuOutputHandler(sm);
+				dstabAlgo.setOutPutPerSteps(1);
+		
+		IpssLogger.getLogger().setLevel(Level.INFO);
+		
+		PerformanceTimer timer = new PerformanceTimer(IpssLogger.getLogger());
+		
+		
 		if (dstabAlgo.initialization()) {
 			System.out.println(dsNet.getMachineInitCondition());
 			
