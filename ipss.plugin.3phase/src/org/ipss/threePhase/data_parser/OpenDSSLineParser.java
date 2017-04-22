8aacdbb742a5a0ac52be64b51f7c9cb672a0ba10
thuang <eetonyhuang@gmail.com>
2017-04-21 23:45:38 -0700

    opendss line data parser

--- ipss.plugin.3phase/src/org/ipss/threePhase/data_parser/OpenDSSLineParser.java
+++ ipss.plugin.3phase/src/org/ipss/threePhase/data_parser/OpenDSSLineParser.java
@@ -20,9 +20,225 @@ public class OpenDSSLineParser {
 		this.dataParser = parser;
 	}
 	
-	public void parseLineString(String lineStr) throws InterpssException{
+	public boolean parseLineData(String lineStr) throws InterpssException{
+		boolean no_error = true;
+    	
+        final String  DOT = ".";
 		
-		final String  DOT = ".";
+		String  lineId = "";
+		String  fromBusId = "";
+		String  fromBusPhases  ="1.2.3"; // by default;;
+		String  toBusId = "";
+		String  toBusPhases = "1.2.3"; // by default;
+		String  lineCodeId = "";
+		double  lineLength = 0;
+		int     phaseNum = 3;    // 3 phases by default
+		
+		String  fromBusStr = "";
+		String  toBusStr = "";
+		
+		DStabNetwork3Phase distNet = this.dataParser.getDistNetwork();
+		
+		Bus3Phase fromBus = null, toBus = null;
+	
+		int phaseIdx = -1, lineCodeIdx = -1;
+		
+		double r1= 0,r0 = 0, x1 = 0, x0 = 0, c1 = 0, c0 = 0;
+		
+
+		String[] lineStrAry = lineStr.split("\\s+");
+		
+		for(int i = 0;i<lineStrAry.length;i++){
+			if(lineStrAry[i].contains("Line.")){
+				lineId    = lineStrAry[i].substring(4);
+			}
+			else if(lineStrAry[i].contains("Phases=")){
+				phaseIdx = i;
+				phaseNum  = Integer.valueOf(lineStrAry[i].substring(7));
+			}
+			
+			else if(lineStrAry[i].contains("Bus1=")){
+				fromBusStr = lineStrAry[i].substring(4);
+			}
+			else if(lineStrAry[i].contains("Bus2=")){
+				toBusStr = lineStrAry[i].substring(4);
+			}
+			else if(lineStrAry[i].contains("LineCode")){
+				lineCodeIdx = i;
+				lineCodeId= lineStrAry[i].substring(9);
+			}
+			else if(lineStrAry[i].contains("Length")){
+				lineLength = Double.valueOf(lineStrAry[i].substring(7));
+			}
+			else if(lineStrAry[i].contains("r1=")){
+				r1 = Double.valueOf(lineStrAry[i].substring(3));
+			}
+			else if(lineStrAry[i].contains("r0=")){
+				r0 = Double.valueOf(lineStrAry[i].substring(3));
+			}
+			else if(lineStrAry[i].contains("x1=")){
+				x1 = Double.valueOf(lineStrAry[i].substring(3));
+			}
+			else if(lineStrAry[i].contains("x0=")){
+				x0 = Double.valueOf(lineStrAry[i].substring(3));
+			}
+			else if(lineStrAry[i].contains("c1=")){
+				c1 = Double.valueOf(lineStrAry[i].substring(3));
+			}
+			else if(lineStrAry[i].contains("c0=")){
+				c0 = Double.valueOf(lineStrAry[i].substring(3));
+			}
+			
+			
+		}
+		
+//		if(phaseIdx>0)
+//		     phaseNum  = Integer.valueOf(lineStrAry[phaseIdx].substring(7));
+//		
+//		lineId    = lineStrAry[lineIdIdx].substring(4);
+//		fromBusStr = lineStrAry[fromBusIdx].substring(4);
+//		toBusStr   = lineStrAry[toBusIdx].substring(4);
+//		lineCodeId = lineStrAry[lineCodeIdx].substring(9);
+//		lineLength = Double.valueOf(lineStrAry[lengthIdx].substring(7));		
+		
+		//busId is the substring before the first DOT
+		//phases info is defined in the substring after the first DOT
+		if(fromBusStr.contains(DOT)){
+			fromBusId = fromBusStr.substring(0, fromBusStr.indexOf(DOT));
+		    fromBusPhases = fromBusId = fromBusStr.substring( fromBusStr.indexOf(DOT)+1);
+		}else{
+			fromBusId = fromBusStr;
+			
+		}
+		
+		if(toBusStr.contains(DOT)){
+			toBusId = toBusStr.substring(0, toBusStr.indexOf(DOT));
+		    toBusPhases = toBusId = toBusStr.substring( toBusStr.indexOf(DOT)+1);
+		}else{
+			fromBusId = toBusStr;
+			
+		}
+		
+		Complex3x3 zabc = null;
+		Complex3x3 yshuntabc = new Complex3x3();
+		
+		if(lineCodeIdx> 0){ // line parameters defined by line code
+			
+			LineConfiguration config = this.dataParser.getLineConfigTable().get(lineCodeId);
+
+			
+			if(config!=null){
+				zabc = config.getZ3x3Matrix();
+			}
+			else{
+				throw new Error("LineConfiguration definition not found, LineCodeId:"+lineCodeId);
+			}
+		}
+		else{ // line parameters defined by raw data
+			if(r1>= 0 & x1>0){
+		
+				Complex z1 = new Complex(r1,x1);
+				Complex z0 = new Complex(r0,x0);
+				
+				zabc = new Complex3x3(z1,z1,z0);
+			}
+			else{
+				throw new Error("Error in Line Z, Y parameter raw data: "+lineStr);
+			}
+			
+		}
+		
+		if(!fromBusPhases.equals(toBusPhases)){
+			throw new Error("different phase arrangements on both terminals not support yet, from: "+fromBusPhases+ ", to: "+toBusPhases);
+		}
+		if(phaseNum==3){
+			// no change is needed
+		}
+		else if(phaseNum==2){
+			if(fromBusPhases.equals("1.2")){
+				//no change is needed
+			}
+			else if (fromBusPhases.equals("1.3")){
+				//no change is needed
+				zabc.ac = zabc.ab;
+				zabc.ab = new Complex(0.0);
+				
+				zabc.ca = zabc.ba;
+				zabc.ba = new Complex(0.0);
+				
+				zabc.cc = zabc.bb;
+				zabc.bb = new Complex(0.0);
+			}
+			else if (fromBusPhases.equals("2.3")){
+				
+				zabc.cc = zabc.bb;
+				
+				zabc.bb = zabc.aa;
+				zabc.aa = new Complex(0.0);
+				
+				zabc.bc = zabc.ab;
+				zabc.ab = new Complex(0.0);
+				
+				zabc.cb = zabc.ba;
+				zabc.ba = new Complex(0.0);
+				
+			}
+			else{
+				throw new Error("phase arrangement not support yet : "+fromBusPhases);
+			}
+		}
+		else if(phaseNum==1){
+			// by default, phase = "1"
+			
+			if(fromBusPhases.equals("2")){
+				zabc.bb = zabc.aa;
+				zabc.aa = new Complex(0.0);
+			}
+			else if(fromBusPhases.equals("3")){
+				zabc.cc = zabc.aa;
+				zabc.aa = new Complex(0.0);
+			}
+			else{
+				throw new Error("phase arrangement not support yet : "+fromBusPhases);
+			}
+		}
+		else{
+			throw new Error("phase number must be 1, 2 or 3");
+		}
+						
+			
+			
+			
+		
+		// line parameters defined by raw input data
+	
+		
+		if(distNet.getBus(fromBusId)==null)
+			fromBus = ThreePhaseObjectFactory.create3PDStabBus(fromBusId, distNet);
+		
+		if(distNet.getBus(toBusId)==null)
+			toBus = ThreePhaseObjectFactory.create3PDStabBus(toBusId, distNet);
+		
+		Branch3Phase line = ThreePhaseObjectFactory.create3PBranch(fromBusId, toBusId, "1", distNet);
+		line.setBranchCode(AclfBranchCode.LINE);
+		// the format of Zmatrix need to be consistent with the number of phases and the phases in use.
+		
+		
+		
+		line.setZabc(zabc);
+		
+		//TODO ShuntY is not considered for this initial implementation 
+		//line.setFromShuntYabc(yshuntabc.multiply(0.5));
+		//line.setToShuntYabc(yshuntabc.multiply(0.5));
+		
+		return no_error;
+		
+	}
+	
+	private boolean parseLineDataWithoutLineCode(String lineStr) throws InterpssException{
+		boolean no_error = true;
+		
+        final String  DOT = ".";
 		
 		String  lineId = "";
 		String  fromBusId = "";
@@ -79,18 +295,6 @@ public class OpenDSSLineParser {
 			fromBusId = toBusStr;
 			
 		}
-		
-		
-		
-		if(distNet.getBus(fromBusId)==null)
-			fromBus = ThreePhaseObjectFactory.create3PDStabBus(fromBusId, distNet);
-		
-		if(distNet.getBus(toBusId)==null)
-			toBus = ThreePhaseObjectFactory.create3PDStabBus(toBusId, distNet);
-		
-		Branch3Phase line = ThreePhaseObjectFactory.create3PBranch(fromBusId, toBusId, "1", distNet);
-		line.setBranchCode(AclfBranchCode.LINE);
-		// the format of Zmatrix need to be consistent with the number of phases and the phases in use.
 		
 		LineConfiguration config = this.dataParser.getLineConfigTable().get(lineCodeId);
 		Complex3x3 zabc = null;
@@ -162,12 +366,22 @@ public class OpenDSSLineParser {
 			throw new Error("LineConfiguration definition not found, LineCodeId:"+lineCodeId);
 		}
 		
+		
+		if(distNet.getBus(fromBusId)==null)
+			fromBus = ThreePhaseObjectFactory.create3PDStabBus(fromBusId, distNet);
+		
+		if(distNet.getBus(toBusId)==null)
+			toBus = ThreePhaseObjectFactory.create3PDStabBus(toBusId, distNet);
+		
+		Branch3Phase line = ThreePhaseObjectFactory.create3PBranch(fromBusId, toBusId, "1", distNet);
+		line.setBranchCode(AclfBranchCode.LINE);
+		// the format of Zmatrix need to be consistent with the number of phases and the phases in use.
+		
+		
 		line.setZabc(zabc);
 		
-		//TODO ShuntY is not considered for this initial implementation 
-		//line.setFromShuntYabc(yshuntabc.multiply(0.5));
-		//line.setToShuntYabc(yshuntabc.multiply(0.5));
 		
+		return no_error;
 	}
 	
 	
