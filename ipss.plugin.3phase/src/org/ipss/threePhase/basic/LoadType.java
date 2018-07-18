177ae03d984761fdd5045f01d11aee1170343a34
jinjingfarm <mzhou@interpss.org>
2018-07-18 14:29:06 +0800

    removed the two classes, since there are available in the core model

--- ipss.plugin.3phase/src/org/ipss/threePhase/basic/LoadType.java
+++
@@ -1,7 +0,0 @@
-package org.ipss.threePhase.basic;
-
-public enum LoadType {
-	
-	Single_Phase, Three_Phase_Wye, Three_Phase_Delta;
-
-}
--- ipss.plugin.3phase/src/org/ipss/threePhase/basic/Phase.java
+++
@@ -1,7 +0,0 @@
-package org.ipss.threePhase.basic;
-
-public enum Phase {
-	
-	A,B,C
-
-}
